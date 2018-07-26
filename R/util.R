46c1074263c944c824c075093ec7d3952775120b
hhau <a.manderson@live.co.uk>
2018-07-26 14:29:39 +0800

    add dated_filename method
    useful for plots

--- R/util.R
+++ R/util.R
@@ -33,3 +33,28 @@
   return(res)
   
 }
+
+#' Date a filename 
+#' 
+#' This function will create the directories in the full file path it 
+#' constructs, and return the full file path to the specified file.
+#' 
+#'
+#' @param filename the filename, with file extensions
+#' @param folder_path the directory for the file, defaults to "./plots/"
+#' @param date a date string, defaults to Sys.Date()
+#' @param sep seperator character, if set to "" and combined with a leading "/"
+#' in `filename`, then dated directories will be created.
+#'
+#' @return a filepath/filename string
+#' @export
+dated_filename <- function(filename, folder_path = "./plots/", date = Sys.Date(), sep = '_') {
+  full_path <- file.path(folder_path, sprintf('%s%s%s', date, sep, filename))
+  
+  if (!dir.exists(dirname(full_path))) {
+    dir.create(dirname(full_path), showWarnings = FALSE, recursive = TRUE)
+  }
+  
+  return(full_path)
+  
+}
