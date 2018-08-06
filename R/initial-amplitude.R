823d323123ff1092edf07b78b8d87c9c96aefeca
hhau <a.manderson@live.co.uk>
2018-08-06 10:35:05 +0800

    added a0 ppd sampler

--- R/initial-amplitude.R
+++ R/initial-amplitude.R
@@ -21,3 +21,44 @@ initial_amplitude_model <- function(stan_data_list, ...) {
                                ...)
   return(model_fit)
 }
+
+#' Sample the PPD for the initial amplitude model
+#' 
+#' Currently only works in settings where 
+#' \code{\link[parallel]{mclapply}} functions.
+#'
+#' @param sea_surface_height the sea surface height
+#' @param n_samples defaults to 500, how many samples to return
+#'
+#' @return vector of legnth n_samples
+#' @export
+a0_sample_generator <- function(sea_surface_height, n_samples = 500) {
+  q <- 3 
+  
+  stan_dat <- list(
+    N = length(a0_data_no_zeros$a_zero),
+    ssh = a0_data_no_zeros$sea_surface_height,
+    a_zero = a0_data_no_zeros$a_zero,
+    N_ssh_new = 1,
+    ssh_new = array(c(sea_surface_height), dim = c(1)),
+    poly_deg = q
+  )
+  
+  res <- parallel::mclapply(1:2, function(x) {
+    capture.output({
+      model_fit <- ddcurves2::initial_amplitude_model(
+        stan_dat,
+        warmup = 175,
+        iter = 175 + (n_samples / 2),
+        cores = 1,
+        chains = 1,
+        refresh = -1
+      )
+      pred_samples <- rstan::extract(model_fit, pars = "a_zero_new_orig")[[1]]
+    })
+    return(pred_samples)
+  })
+  
+  return(unlist(res))
+  
+}
