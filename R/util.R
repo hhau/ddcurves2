ca683b35df7b54283b279c79e20df054483e3c13
Andrew Manderson <a.manderson@live.co.uk>
2019-03-01 11:03:17 +0000

    Add initial value jitterer - roughy and loopy, but works.

--- R/util.R
+++ R/util.R
@@ -58,3 +58,58 @@ dated_filename <- function(filename, folder_path = "./plots/", date = Sys.Date()
   return(full_path)
   
 }
+
+# take an init list, return a jittered version of it
+# where each element has jittered by its value * jitter_magnitude
+# e.g.: init_jitter(list(a = 1), 0.01) 
+#   list(a = with(init_list,
+#    runif(
+#    1,
+#    lower = a - (a * jiiter_magnitude / 2),
+#    upper = a + (a * jiiter_magnitude / 2)
+#    ))
+#' Init jitterer
+#' 
+#' Jitters the initial values for a list of initial values that is in the form
+#' required by \code{\link[rstan:stanmodel-method-sampling]{sampling}}. 
+#' \code{jitter_magnitude} controls the magnitude of the jittering, i.e. An 
+#' initial value of 1 with \code{jitter_magnitude} = 0.001 will get jittered to
+#' be uniformly distributed on the interval between 1 +/- (1 * 0.001 / 2).
+#' 
+#' @param init_list A list of initial values.
+#' @param jitter_magnitude Magnitude of the jittering.
+#'
+#' @return A jittered version of \code{init_list}.
+#' @export
+init_jitter <- function(init_list, jitter_magnitude = 0.001) {
+  n_params <- length(init_list)
+  for (par in seq_len(n_params)) {
+    sub_element <- as.array(init_list[[par]])
+    sub_dims <- dim(sub_element)
+    ## hard code for 1d & 2d here
+    if (length(sub_dims) == 1) {
+      for (val in seq_len(length(sub_element))) {
+        magnitude <- sub_element[val]
+        dist <- magnitude * jitter_magnitude / 2 
+        init_list[[par]][val] <- runif(
+          n = 1,
+          min = magnitude - dist,
+          max = magnitude + dist
+        )
+      }
+    } else if (length(sub_dims) == 2) {
+      for (col in seq_len(sub_dims[2])) {
+        for (row in seq_len(sub_dims[1])) {
+          magnitude <- sub_element[row, col]
+          dist <- magnitude * jitter_magnitude / 2 
+          init_list[[par]][row, col] <- runif(
+            n = 1,
+            min = magnitude - dist,
+            max = magnitude + dist
+          )
+        }
+      }
+    }
+  }
+  return(init_list)  
+}
